babea06c6779af9edca1e209d1c42c7ddaf00966
Carlos Matheus <carlos.matheus@outlook.com>
2023-11-15 23:55:40 -0300

    Arquivos de configuração iniciais do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeNumberProject;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira o número que gostaria de checar: ");
+        int numero;
+        numero = Convert.ToInt32(Console.ReadLine());
+        int divisores = 0;
+    }
+}
